f18630f34ab630a27764eb8d31a35d24c0330bd1
lisanbao01 <lisanbao01@baidu.com>
2026-09-08 21:57:41 +0800

    [kunlun]resolve copysign_/copysign_out performance

--- src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
@@ -101,6 +101,7 @@ from .conv3d import conv3d
 from .conv_depthwise2d import _conv_depthwise2d
 from .copy import copy, copy_
 from .copysign import copysign, copysign_out
+from .copysign_ import copysign_
 from .cos import cos, cos_
 from .count_nonzero import count_nonzero
 from .cummax import cummax
